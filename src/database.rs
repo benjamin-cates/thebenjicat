29c306a8374c1d922171f312c067bef29e7cf57e
Benjamin Cates <thebenjicat@gmail.com>
2024-01-04 19:38:10 -0800

    Remove unwraps from database.rs

--- src/database.rs
+++ src/database.rs
@@ -12,39 +12,42 @@ pub struct FileListing {
 impl FileListing {
     pub fn from_path(path: &str, db: &Mutex<Connection>) -> Option<Self> {
         db.lock()
-            .unwrap()
+            .ok()?
             .query_row(
                 "SELECT id, path, password FROM files WHERE path=?1",
                 params![path],
                 |row| {
                     Ok(Self {
-                        id: row.get_unwrap(0),
-                        path: row.get_unwrap(1),
-                        password: row.get_unwrap(2),
+                        id: row.get(0)?,
+                        path: row.get(1)?,
+                        password: row.get(2)?,
                     })
                 },
             )
             .ok()
     }
     pub fn read_all(db: &Mutex<Connection>) -> Result<Vec<Self>, rusqlite::Error> {
-        let binding = db.lock().unwrap();
+        let binding = db.lock().ok().ok_or(rusqlite::Error::InvalidQuery)?;
         let mut stmt = binding.prepare("SELECT id, path, password FROM files")?;
         let mut rows = stmt.query([])?;
         let mut out = Vec::new();
         while let Some(row) = rows.next()? {
             out.push(FileListing {
-                id: row.get_unwrap(0),
-                path: row.get_unwrap(1),
-                password: row.get_unwrap(2),
+                id: row.get(0)?,
+                path: row.get(1)?,
+                password: row.get(2)?,
             });
         }
         Ok(out)
     }
     pub fn push_to_db(&self, db: &Mutex<Connection>) -> Result<usize, rusqlite::Error> {
-        db.lock().unwrap().execute(
-            "INSERT INTO files (path, password) VALUES (?1,?2)",
-            params![self.path, self.password],
-        )
+        db.lock()
+            .ok()
+            .ok_or(rusqlite::Error::InvalidQuery)?
+            .execute(
+                "INSERT INTO files (path, password) VALUES (?1,?2)",
+                params![self.path, self.password],
+            )
     }
 }
 
@@ -59,41 +62,44 @@ pub struct LinkListing {
 impl LinkListing {
     pub fn from_path(path: &str, db: &Mutex<Connection>) -> Option<Self> {
         db.lock()
-            .unwrap()
+            .ok()?
             .query_row(
                 "SELECT id, path, name, destination FROM links WHERE path=?1",
                 params![path],
                 |row| {
                     Ok(Self {
-                        id: row.get_unwrap(0),
-                        path: row.get_unwrap(1),
-                        name: row.get_unwrap(2),
-                        destination: row.get_unwrap(3),
+                        id: row.get(0)?,
+                        path: row.get(1)?,
+                        name: row.get(2)?,
+                        destination: row.get(3)?,
                     })
                 },
             )
             .ok()
     }
     pub fn read_all(db: &Mutex<Connection>) -> Result<Vec<Self>, rusqlite::Error> {
-        let binding = db.lock().unwrap();
+        let binding = db.lock().ok().ok_or(rusqlite::Error::InvalidQuery)?;
         let mut stmt = binding.prepare("SELECT id, path, name, destination FROM links")?;
         let mut rows = stmt.query([])?;
         let mut out = Vec::new();
         while let Some(row) = rows.next()? {
             out.push(LinkListing {
-                id: row.get_unwrap(0),
-                path: row.get_unwrap(1),
-                name: row.get_unwrap(2),
-                destination: row.get_unwrap(3),
+                id: row.get(0)?,
+                path: row.get(1)?,
+                name: row.get(2)?,
+                destination: row.get(3)?,
             });
         }
         Ok(out)
     }
     pub fn push_to_db(&self, db: &Mutex<Connection>) -> Result<usize, rusqlite::Error> {
-        db.lock().unwrap().execute(
-            "INSERT INTO links (path, name, destination) VALUES (?1,?2,?3)",
-            params![self.path, self.name, self.destination],
-        )
+        db.lock()
+            .ok()
+            .ok_or(rusqlite::Error::InvalidQuery)?
+            .execute(
+                "INSERT INTO links (path, name, destination) VALUES (?1,?2,?3)",
+                params![self.path, self.name, self.destination],
+            )
     }
 }
 
